6ba157c5c3f047429928909d729ef17da54bd7b2
jyotishmann <jyotishmann@gmail.com>
2026-09-11 22:04:53 +0530

    feat(p06): row-parallel linear (partial products + ring all-reduce) (#38)

--- src/gpu_roofline/layers/__init__.py
+++ src/gpu_roofline/layers/__init__.py
@@ -1,2 +1,4 @@
 from .tp_linear import (ColumnParallelLinear, partition_weight_column,
       assert_column_parallel_correct)
+from .tp_linear import (RowParallelLinear, sum_allreduce, partition_weight_row,
+      assert_row_parallel_correct)
